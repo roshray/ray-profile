832456deab975ba46d8272c8add1a2c34fc997f0
roshray <roshanroy759@gmail.com>
2022-02-24 20:48:04 +0530

    work-I

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import './Work.scss'
+import { AiFillEye, AiFillGithub } from 'react-icons/ai'
+import { motion} from 'framer-motion'
+
+import { AppWrap } from '../../wrapper'
+import { urlFor, client} from '../../client'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+      <h2 className="head-text">My Creative <span>Portfolio </span>Section</h2>
+    </>
   )
 }
 
