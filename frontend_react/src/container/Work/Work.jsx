c3f1dba166fbc2435e74347d7bc252d9bf25f55d
roshray <roshanroy759@gmail.com>
2022-02-25 20:53:49 +0530

    Skills & Experience Section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -3,7 +3,7 @@ import './Work.scss'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion} from 'framer-motion'
 
-import { AppWrap } from '../../wrapper'
+import { AppWrap, MotionWrap } from '../../wrapper'
 import { urlFor, client} from '../../client'
 
 const Work = () => {
@@ -109,4 +109,8 @@ const Work = () => {
   )
 }
 
-export default AppWrap(Work, 'work')
+export default AppWrap(
+  MotionWrap(Work,'app__works'), 
+  'work',
+  "app__primarybg"    
+)
